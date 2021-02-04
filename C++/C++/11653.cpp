1641dc2ec4869eaef781e95e79ec6876aed0a902
progmins <mingfordev@gmail.com>
2021-02-04 16:57:19 +0900

    Solve 1929

--- C++/C++/11653.cpp
+++ C++/C++/11653.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
-
-using namespace std;
-
-int main(void) {
-	int N;
-	int divNum=2;
-	cin >> N;
-
-	while (N >= divNum) {
-		if (N % divNum == 0) {
-			N = N / divNum;
-			cout << divNum << endl;
-			divNum = 2;
-			continue;
-		}
-		divNum++;
-	}
-}
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main(void) {
+//	int N;
+//	int divNum=2;
+//	cin >> N;
+//
+//	while (N >= divNum) {
+//		if (N % divNum == 0) {
+//			N = N / divNum;
+//			cout << divNum << endl;
+//			divNum = 2;
+//			continue;
+//		}
+//		divNum++;
+//	}
+//}
