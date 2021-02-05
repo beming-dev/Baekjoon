04b7abbecfaf9d0436c36e122cfbcdf10dadef95
progmins <mingfordev@gmail.com>
2021-02-05 16:43:35 +0900

    Solve 1018

--- C++/C++/test.cpp
+++ C++/C++/test.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-
-using namespace std;
-
-
-int main() {
-	cout << -1 << endl;
-}
+//#include<iostream>
+//
+//using namespace std;
+//
+//
+//int main() {
+//	cout << -1 << endl;
+//}
