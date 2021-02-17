8a4626624a24abd0037725fc775dbae27672d96b
progmins <mingfordev@gmail.com>
2021-02-17 18:40:19 +0900

    Solve 1463

--- C++/C++/1463.cpp
+++ C++/C++/1463.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
-#include<algorithm>
-
-using namespace std;
-
-int main(void) {
-	int arr[1000001] = { 0, };
-	int n;
-
-	cin >> n;
-
-	arr[1] = 0;
-	for (int i = 2; i <= n; i++) {
-		arr[i] = arr[i - 1] + 1;
-		if (i % 2 == 0)arr[i] = min(arr[i], arr[i / 2] + 1);
-		if (i % 3 == 0)arr[i] = min(arr[i], arr[i / 3] + 1);
-	}
-
-	cout << arr[n];
-
-	return 0;
-}
+//#include<iostream>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//int main(void) {
+//	int arr[1001] = { 0, };
+//	int n;
+//
+//	cin >> n;
+//
+//	arr[1] = 0;
+//	for (int i = 2; i <= n; i++) {
+//		arr[i] = arr[i - 1] + 1;
+//		if (i % 2 == 0)arr[i] = min(arr[i], arr[i / 2] + 1);
+//		if (i % 3 == 0)arr[i] = min(arr[i], arr[i / 3] + 1);
+//	}
+//
+//	cout << arr[n];
+//
+//	return 0;
+//}
