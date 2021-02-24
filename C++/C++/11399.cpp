0a3347748c40485545d78ef1e48758de68e11149
progmins <mingfordev@gmail.com>
2021-02-24 20:50:23 +0900

    Solve 1541

---
+++ C++/C++/11399.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//int main(void) {
+//	int N;
+//	int in, time=0, sum=0;
+//	vector<int> arr;
+//
+//	cin >> N;
+//
+//	for (int i = 0; i < N; i++) {
+//		cin >> in;
+//		arr.push_back(in);
+//	}
+//
+//	sort(arr.begin(), arr.end());
+//
+//	for (int i = 0; i < N; i++) {
+//		sum += arr[i];
+//		time += sum;
+//	}
+//
+//	cout << time;
+//}
