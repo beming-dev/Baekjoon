4092fa881e56039616295f000bad6005eae22ad2
progmins <mingfordev@gmail.com>
2021-02-26 21:05:38 +0900

    Solve 1037

---
+++ C++/C++/1037.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//int main(void) {
+//	int N, in;
+//	vector<int> medical;
+//
+//	cin >> N;
+//
+//	for (int i = 0; i < N; i++) {
+//		cin >> in;
+//		medical.push_back(in);
+//	}
+//	sort(medical.begin(), medical.end());
+//
+//	cout << medical.front() * medical.back();
+//}
