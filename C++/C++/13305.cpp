a82e019e60a32ab3654ea84eaebd660513829c79
progmins <mingfordev@gmail.com>
2021-02-26 20:52:49 +0900

    Solve 5086

--- C++/C++/13305.cpp
+++ C++/C++/13305.cpp
@@ -1,48 +1,48 @@
-#include<iostream>
-
-using namespace std;
-
-int length[100000] = { 0 };
-int cost[100000] = { 0 };
-
-int main(void) {
-	int N;
-
-	cin >> N;
-
-	for (int i = 0; i < N-1; i++) {
-		cin >> length[i];
-	}
-	for (int i = 0; i < N; i++) {
-		cin >> cost[i];
-	}
-
-	int minCost = 0;
-	int partLength = 0;
-	int j;
-
-	for (int i = 0; i < N; i++) {
-		partLength = 0;
-		for (j = i+1; j < N; j++) {
-			if (cost[j] < cost[i]) {
-				for (int k = i; k < j; k++) {
-					partLength += length[k];
-				}
-				break;
-			}
-		}
-		if (j == N) {
-			for (int k = i; k < N; k++) {
-				partLength += length[k];
-			}
-			minCost += partLength * cost[i];
-			break;
-		}
-		else {
-			minCost += partLength * cost[i];
-			i = j-1;
-		}
-	}
-
-	cout << minCost;
-}
+//#include<iostream>
+//
+//using namespace std;
+//
+//int length[100000] = { 0 };
+//int cost[100000] = { 0 };
+//
+//int main(void) {
+//	int N;
+//
+//	cin >> N;
+//
+//	for (int i = 0; i < N-1; i++) {
+//		cin >> length[i];
+//	}
+//	for (int i = 0; i < N; i++) {
+//		cin >> cost[i];
+//	}
+//
+//	int minCost = 0;
+//	int partLength = 0;
+//	int j;
+//
+//	for (int i = 0; i < N; i++) {
+//		partLength = 0;
+//		for (j = i+1; j < N; j++) {
+//			if (cost[j] < cost[i]) {
+//				for (int k = i; k < j; k++) {
+//					partLength += length[k];
+//				}
+//				break;
+//			}
+//		}
+//		if (j == N) {
+//			for (int k = i; k < N; k++) {
+//				partLength += length[k];
+//			}
+//			minCost += partLength * cost[i];
+//			break;
+//		}
+//		else {
+//			minCost += partLength * cost[i];
+//			i = j-1;
+//		}
+//	}
+//
+//	cout << minCost;
+//}
---
+++ C++/C++/5086.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	int n1, n2;
+
+	while (1) {
+		cin >> n1 >> n2;
+		if (n1 == 0 && n2 == 0)
+			break;
+		if ((n1 > n2) && (n1 % n2 == 0))
+			cout << "multiple" << '\n';
+		else if ((n1 < n2) && (n2 % n1 == 0))
+			cout << "factor" << '\n';
+		else
+			cout << "neither" << '\n';
+	}
+}
