a92cb9601f8d66a396b847e936379a7bc80f9dc9
progmins <mingfordev@gmail.com>
2021-02-27 12:31:30 +0900

    Solve 2981

--- C++/C++/1934.cpp
+++ C++/C++/1934.cpp
@@ -1,1 +1,23 @@
-//sd
+//#include<iostream>
+//
+//using namespace std;
+//
+//int Euclid(int a, int b) {
+//	if (a%b == 0) {
+//		return b;
+//	}
+//	return Euclid(b, a % b);
+//}
+//
+//int main(void) {
+//	int T;
+//	int n1, n2;
+//
+//	cin >> T;
+//
+//	for (int i = 0; i < T; i++) {
+//		cin >> n1 >> n2;
+//		int ans = Euclid(n1, n2);
+//		cout << n1 * n2/ans << '\n';
+//	}
+//}
