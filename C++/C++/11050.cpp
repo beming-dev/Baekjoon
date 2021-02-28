cddb040faac5b86efae497b175feb03df0ebd11d
progmins <mingfordev@gmail.com>
2021-02-28 22:21:41 +0900

    Create rain box project

--- C++/C++/11050.cpp
+++ C++/C++/11050.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std;
-
-int n, k, c[1001][1001];
-int main() {
-    cin >> n >> k;
-    c[0][0] = 1;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= k; j++) {
-            if (j - 1 < 0)c[i][j] = c[i - 1][j];
-            c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % 10007;
-        }
-    }
-    printf("%d", c[n][k]);
-}
-
-
+//#include <iostream>
+//using namespace std;
+//
+//int n, k, c[1001][1001];
+//int main() {
+//    cin >> n >> k;
+//    c[0][0] = 1;
+//    for (int i = 1; i <= n; i++) {
+//        for (int j = 0; j <= k; j++) {
+//            if (j - 1 < 0)c[i][j] = c[i - 1][j];
+//            c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % 10007;
+//        }
+//    }
+//    printf("%d", c[n][k]);
+//}
+//
+//
 
 
 
